f117362e9713fbc3691725e1067791060698b596
Kaio Vinicius <kaio.pereira@varsolutions.com.br>
2023-05-22 16:19:36 -0300

    Adicionar arquivos de projeto.

---
+++ Controllers/AutenticacaoController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+using System;
+using System.IdentityModel.Tokens.Jwt;
+using System.Text;
+using WebAPIPessoa.Application.Autenticacao;
+using WebAPIPessoa.Repository;
+
+namespace WebAPIPessoa.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class AutenticacaoController : ControllerBase
+    {
+        private readonly PessoaContext _context;
+
+        public AutenticacaoController(PessoaContext context)
+        {
+            _context = context;
+        }
+        [HttpPost]
+        public IActionResult Login([FromBody] AutenticacaoRequest request)
+        {
+            var autenticacaoService = new AutenticacaoService(_context);//Ele esta pegando todos codigos da "AutenticacaoService", e jogando dentro da variavel
+            var resposta = autenticacaoService.Autenticar(request);
+
+            if (resposta == null) 
+            {
+                return Unauthorized();
+            }
+
+            else
+            {
+                return Ok(resposta);
+            }
+
+        }
+
+    }
+}
